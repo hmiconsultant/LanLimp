a33ca3590d3ef063a365228521d0948860495e7a
Gustavo Dutra <gustavoddutra@gmail.com>
2020-12-17 19:01:20 -0300

    Corrigir acesso ao menu mobile
    O menu não estava gerando nenhuma ação ao acessar a partir de um
    navegador mobile.
    
    Para corrigir, foi necessário remover do javascript o método
    "e.preventDefault()".

--- js/action.js
+++ js/action.js
@@ -11,8 +11,9 @@ $(document).ready(function() {
 
     // make it as accordion for smaller screens
     if ($(window).width() < 992) {
+
         $('.dropdown-menu a').click(function(e) {
-            e.preventDefault();
+            //e.preventDefault();
             if ($(this).next('.submenu').length) {
                 $(this).next('.submenu').toggle();
             }
